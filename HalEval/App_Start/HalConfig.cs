fb34cec1cdb60e4ed9d7a1f1f679de6b2eeced78
Paul Rohorzka <paul.rohorzka@techtalk.at>
2014-10-03 20:44:27 +0200

    First End-To-End test

---
+++ HalEval/App_Start/HalConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Http;
+using WebApi.Hal;
+
+namespace HalEval
+{
+    public static class HalConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.Formatters.Add(new JsonHalMediaTypeFormatter());
+            config.Formatters.Add(new XmlHalMediaTypeFormatter());
+        }
+    }
+}
